2ab7c09b7aef2afb2a9b0749b2cfbe736a894e6f
Ynno Plucena <101958912+Ynnocent@users.noreply.github.com>
2026-02-03 21:27:22 -0700

    Add socket.io-client dependency and update CustomComponent

--- app/Components/customComponent.tsx
+++ app/Components/customComponent.tsx
@@ -1,7 +1,12 @@
+import {io} from "socket.io-client";
+
+const socket = io("http://localhost:3000/")
+
 export const CustomComponent = () => {
     return(
         <>
         <div>
+            
             <h1>Fuck this shit</h1>
         </div>
         </>
